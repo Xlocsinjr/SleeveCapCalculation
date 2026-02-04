34d73917816bb2421bbc0ba5c712a5e7d5638c09
Xander Locsin <31654108+Xlocsinjr@users.noreply.github.com>
2026-02-04 18:05:06 +0100

    Added html main view and accompanying css and js files. Main view has its styling and general layout.

---
+++ scripts/visualisation.js
@@ -0,0 +1,39 @@
+/**
+ * visualisation.js
+ *
+ * This script creates a world map, a scatterplot and a bar chart showing data
+ * from allData.json in progProject.html. This script is loaded by
+ * progProject.html to create the charts.
+ *
+ * Sources:
+ *  https://www.w3schools.com/howto/howto_js_rangeslider.asp
+ *    for the slider.
+ *  https://www.w3schools.com/howto/howto_js_dropdown.asp
+ *    for the dropdown menu.
+ *
+ * Author: Xander Locsin
+ * StudentID: 10722432
+ */
+
+
+// =================== INITIALISATIONS =========================================
+
+// Copied from https://www.w3schools.com/howto/howto_js_rangeslider.asp
+// Looks for the textarea in the document.
+var inputField = document.getElementById("InputField");
+var calcButton = document.getElementById("InputField");
+
+// =================== MAIN FUNCTION DATA LOAD =================================
+
+function main() {
+    
+};
+
+// =================== FUNCTIONS ===============================================
+
+
+
+// ------------------- WHEN LOADED ---------------------------------------------
+window.onload = function() {
+  main();
+};
