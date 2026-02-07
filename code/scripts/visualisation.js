2b7da4db0d83f62febdcec3e2b07885300605ab0
Xander Locsin <Xlocsinjr@gmail.com>
2026-02-07 18:51:31 +0100

    Added the calculation of sleeve cap wavelength and amplitude to function DoCalculation.

--- code/scripts/visualisation.js
+++ code/scripts/visualisation.js
@@ -24,38 +24,129 @@ var inputField = document.getElementById("InputField");
 var calcButton = document.getElementById("calcButton");
 var heightResultBox = document.getElementById("HeightResult");
 var widthResultBox = document.getElementById("WidthResult");
+var waveFunctionDiv = document.getElementById("WaveFunction");
+var amplitudeResultBox = document.getElementById("AmplitudeResult");
+var wavelengthResultBox = document.getElementById("WavelengthResult");
 
-const nanResponse = "Input is not numeric!"
+const nanResponse = "Input is not numeric!";
+const defaultWaveFunction = "$$ y(x) = A \\; sin (\\frac{2 \\pi}{ \\lambda } \\; x ) $$";
+const dx = 0.1;
+const bigDWvl = 1.0
+const smallDWvl = 0.01
+const wvlAccuracy = 0.01
 
 // =================== MAIN FUNCTION DATA LOAD =================================
 
 function main() {
-    heightResultBox.contentEditable = false;
-    widthResultBox.contentEditable = false;
+  heightResultBox.contentEditable = false;
+  widthResultBox.contentEditable = false;
 }
 
 // =================== FUNCTIONS ===============================================
 
 function DoCalculation() {
   // Clear result boxes.
-  heightResultBox.textContent = nanResponse;
-  widthResultBox.textContent = nanResponse;
+  heightResultBox.textContent = "";
+  widthResultBox.textContent = "";
+  waveFunctionDiv.innerHTML = defaultWaveFunction;
+  MathJax.typeset();
 
-  // Check if user input can be parsed to float.
+  // Gather user input.
   var userInput = inputField.value;
-  var isNumeric = parseFloat(userInput)
-  if (!isNumeric) {
+  var circumference = parseFloat(userInput)
+  // Check if user input can be parsed to float.
+  if (!circumference) {
     heightResultBox.textContent = nanResponse;
     widthResultBox.textContent = nanResponse;
     return
   }
 
-  // Calculate
-  heightResultBox.textContent = "Test";
-  widthResultBox.textContent = "Test";
+  // Calculate Amplitude.
+  // C = pi D,    => D = C / pi
+  // 2a^2 = D^2,  => a = sqrt(D^2 / 2)
+  // A = a / 2   = sqrt(D^2 / 2) / 2 = sqrt( (C / pi)^2 / 2) / 2
+  var sleeveCapHeight = Math.sqrt(Math.pow(circumference / Math.PI, 2) / 2);
+  var amplitude = sleeveCapHeight / 2;
+  heightResultBox.textContent = sleeveCapHeight.toFixed(2);
+  amplitudeResultBox.textContent = amplitude.toFixed(2);
+
+  // Calculate wavelength.
+  var wavelength = NaN;
+  var found = false;
+  var overshotWvl = NaN;
+
+  // Loop through different wavelengths 
+  // and checks if the resulting arclength matches the armhole circumference.
+  for (var trialWvl = bigDWvl; trialWvl < 100; trialWvl += bigDWvl) {
+    widthResultBox.textContent = `Checking ${trialWvl} ...`;
+
+    var arcLength = calc_arclength(amplitude, trialWvl);
+
+    if (circumference < arcLength + wvlAccuracy && circumference > arcLength - wvlAccuracy) {
+      wavelength = trialWvl;
+      found = true;
+      break;
+    }
+
+    // Break if overshot.
+    if (arcLength > circumference) {
+      overshotWvl = trialWvl;
+      break;
+    }
+  }
+
+  // If overshot, loop back in opposite direction with smaller interval
+  if (!found) {
+    for (var smallTrialWvl = overshotWvl - smallDWvl; smallTrialWvl >= overshotWvl - bigDWvl; smallTrialWvl -= smallDWvl) {
+      widthResultBox.textContent = `Checking ${smallTrialWvl} ...`;
+      var arcLength = calc_arclength(amplitude, smallTrialWvl);
+
+      if (circumference < arcLength + wvlAccuracy && circumference > arcLength - wvlAccuracy) {
+        wavelength = smallTrialWvl;
+        found = true;
+        break;
+      }
+    }
+  }
+
+  if (!found) {
+    widthResultBox.textContent = "Could not determine wavelength :(";
+    return;
+  }
+
+  // Display wavefunction.
+  var ampString = amplitude.toFixed(2);
+  var wvlString = wavelength.toFixed(2);
+  widthResultBox.textContent = wvlString;
+  wavelengthResultBox.textContent = wvlString;
+
+  waveFunctionDiv.innerHTML = `$$ y(x) = ${ampString} \\; sin (\\frac{2 \\pi}{ ${wvlString} } \\; x ) $$`;
+  MathJax.typeset();
+}
+
+// -----------------------------------------------------------------------------
+
+function wave(A, wvl, x) {
+  return A * Math.sin((2 * Math.PI * x) / wvl);
+}
+
+function dy(A, wvl, x) {
+  return wave(A, wvl, x + dx) - wave(A, wvl, x);
+}
+
+function arcpart(A, wvl, x) {
+  return Math.sqrt(Math.pow(dy(A, wvl, x), 2) + Math.pow(dx, 2));
+}
+
+function calc_arclength(A, wvl) {
+  var L = 0;
+  for (var x = dx; x <= wvl; x += dx) {
+    L += arcpart(A, wvl, x)
+  }
+  return L;
 }
 
 // ------------------- WHEN LOADED ---------------------------------------------
-window.onload = function() {
+window.onload = function () {
   main();
 };
