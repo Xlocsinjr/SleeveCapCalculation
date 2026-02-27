8fddb883145d027b339097b755b9f302a2d10254
locsinjr <x.locsinjr@amolf.nl>
2026-02-27 13:44:43 +0100

    Added Main function. Fits a line over the armhole circumferences and their corresponding numerically calculated arclengths.

--- code/scripts/WvlToCircPlot.py
+++ code/scripts/WvlToCircPlot.py
@@ -1,8 +1,55 @@
 import math as m
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 dWvl = 0.001
+
+
+def Main():
+    wvlList = []
+    circList = []
+    lastFound = 0.001
+
+    for circumference in np.arange(1, 100, 1):
+        circList.append(circumference)
+
+        found = False
+        # pi D = C => D = C / pi
+        D = circumference / m.pi
+        #2a^2 = D^2 => a = sqrt( D^2 / 2)
+        # amplitude = 1/2 * a
+        amplitude = 0.5 * m.sqrt( (D**2) / 2)
+
+        for wavelength in np.arange(lastFound, 100, dWvl):
+            arcLength = calc_arclength(amplitude, wavelength)
+            if (circumference < (arcLength + 0.1) and circumference > (arcLength - 0.1)):
+                wvlList.append(wavelength)
+                lastFound = wavelength
+                found = True
+                break
+        if not found:
+                wvlList.append(0)
+                wavelength = 0
+        print(f"circ={circumference}, then A={amplitude}, wvl={wavelength}")
+
+
+    # It's a line !!!!
+    params = curve_fit(line, circList, wvlList)
+    slope = params[0]
+
+    print(f"slope = {slope}")
+    # The slope is 0.86883832
+
+    fit_xs = [1, 100]
+    fit_ys = [line(1, slope), line(100, slope)]
+
+    plt.plot(circList, wvlList)
+    plt.plot(fit_xs, fit_ys)
+    plt.show()
+
+
+# ======================================================================================
 
 def wave(A, wvl, x):
     return A * m.sin(((2*m.pi)/wvl) * x )
@@ -28,39 +75,12 @@ def calc_arclength(A, wvl):
         L += arcpart(A, wvl, x, dx)
     return L
 
-wvlList = []
-circList = []
-lastFound = 0.001
 
-
-for circumference in np.arange(1, 100, 1):
-    circList.append(circumference)
-
-    found = False
-    # pi D = C => D = C / pi
-    D = circumference / m.pi
-    #2a^2 = D^2 => a = sqrt( D^2 / 2)
-    # amplitude = 1/2 * a
-    amplitude = 0.5 * m.sqrt( (D**2) / 2)
-
-    for wavelength in np.arange(lastFound, 100, dWvl):
-        arcLength = calc_arclength(amplitude, wavelength)
-        if (circumference < (arcLength + 0.1) and circumference > (arcLength - 0.1)):
-            wvlList.append(wavelength)
-            lastFound = wavelength
-            found = True
-            break
-    if not found:
-            wvlList.append(0)
-            wavelength = 0
-    print(f"circ={circumference}, then A={amplitude}, wvl={wavelength}")
-
-
-plt.plot(circList, wvlList)
-plt.show()
-
-# It's a line !!!!
+def line(x, slope):
+    return slope * x
 
 
 
 
+if __name__ == "__main__":
+    Main()
